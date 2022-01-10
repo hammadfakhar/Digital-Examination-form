c1bf055abd0515b60f6e8c89b2c3740b7ba342c2
Syed Hammad ul nain fakhar <hammadfakhar51@gmail.com>
2022-01-10 22:19:26 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,107 @@
+// var displayValue1 = document.getElementById("01")
+// var displayvalue2 = document.getElementById("02")
+// var displayValue3 = document.getElementById("03")
+// var displayValue4 = document.getElementById("04")
+
+// var candidateName = document.getElementById("fname")
+// var fname = document.getElementById("fathername")
+// var address = document.getElementById("residential")
+// var birthDate1 = document.getElementById("dob1")
+// var birthDate2 = document.getElementById("dob2")
+// var birthMonth1 = document.getElementById("dob3")
+// var birthMonth2 = document.getElementById("dob4")
+// var birthYear1 = document.getElementById("dob5")
+// var birthYear2 = document.getElementById("dob6")
+// var nationality = document.getElementById("nation")
+// var gender = document.getElementById("gender")
+
+// var cnic = document.getElementById("nic")
+// var contact = document.getElementById("contact")
+// var religion = document.getElementById("religion")
+
+// var mark = document.getElementById("moi")
+// var rollNumber = document.getElementById("roll")
+
+
+// var tboard = document.getElementById("board")
+// var trollNo = document.getElementById("roll no")
+// var tgroup = document.getElementById("group")
+// var tyear = document.getElementById("year")
+// var tmarks = document.getElementById("marks")
+
+
+// var tboard1 = document.getElementById("board1")
+// var trollNo1 = document.getElementById("roll no1")
+// var tgroup1 = document.getElementById("group1")
+// var tyear1 = document.getElementById("year1")
+// var tmarks1 = document.getElementById("marks1")
+
+
+// var checkbox1 = document.getElementById("checkbox1")
+// var checkbox2 = document.getElementById("checkbox2")
+// var checkbox3 = document.getElementById("checkbox3")
+// var checkbox4 = document.getElementById("checkbox4")
+// var checkbox5 = document.getElementById("checkbox5")
+// var checkbox6 = document.getElementById("checkbox6")
+// var checkbox7 = document.getElementById("checkbox7")
+// var checkbox8 = document.getElementById("checkbox8")
+// var checkbox9 = document.getElementById("checkbox9")
+// var checkbox10 = document.getElementById("checkbox10")
+
+// function getValue(){
+//     var a = displayValue1.value;
+//     var b = displayvalue2.value;
+//     var c = displayValue3.value;
+//     var d = displayValue4.value;
+//     var getCandidateName = candidateName.value;
+
+//     var fatname = fname.value;
+//     var add = address.value;
+//     var bd1 = birthDate1.value
+//     var bd2 = birthDate2.value
+//     var bd3 = birthMonth1.value
+//     var bd4=  birthMonth2.value
+//    var bd5 = birthYear1.value
+//    var bd6 = birthYear2.value
+//    var nation =  nationality.value
+//    var gend = gender.value
+//    var nic = cnic.value
+//    var number =  contact.value
+//    var religion1 =  religion.value
+//    var score = mark.value
+//    var rNum = rollNumber.value
+
+//    var e = tboard.value
+//    var f = trollNo.value
+//    var g = tgroup.value
+//    var h = tyear.value
+//    var i = tmarks.value
+//    var j = tboard1.value
+//    var k = trollNo1.value
+//    var l = tgroup1.value
+//    var m = tyear1.value
+//    var n = tmarks1.value
+    
+//    var o = checkbox1.value
+//    var p = checkbox2.value
+//    var q = checkbox3.value
+//    var r = checkbox4.value
+//    var s = checkbox5.value
+//    var t = checkbox6.value
+//    var u = checkbox7.value
+//    var v = checkbox8.value
+//    var w = checkbox9.value
+//    var x = checkbox10.value
+
+
+//      var UserYear = a + b + c + d
+//      console.log(UserYear + getCandidateName)
+// }
+
+function formhide(){
+    var z= document.getElementsByClassName("container")
+    z.style.display = 'none';
+
+    console.log(z)
+}
+
